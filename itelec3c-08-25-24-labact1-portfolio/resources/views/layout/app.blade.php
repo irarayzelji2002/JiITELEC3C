44a6c7345e9abf1516bd677067725dab69ae77e5
Ira Rayzel S. Ji <103237256+irarayzelji2002@users.noreply.github.com>
2024-08-25 19:38:10 +0800

    with bootstrap

--- itelec3c-08-25-24-labact1-portfolio/resources/views/layout/app.blade.php
+++ itelec3c-08-25-24-labact1-portfolio/resources/views/layout/app.blade.php
@@ -4,7 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="../css/app.css">
+    <!-- Boostrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
 </head>
 <body>
     @include('layout.navbar')
@@ -16,5 +19,7 @@
     @include('layout.footer')
 
     <script src="{{ asset('js/app.js') }}"></script>
+    <!-- Boostrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
